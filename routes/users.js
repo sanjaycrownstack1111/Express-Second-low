e06c7f7828f5951a2bc0f57b072b1c5b275adc3c
sanjay@crownstack <sanjay.barman@crownstack.com>
2021-05-07 14:19:38 +0530

    crete singnup/register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(express.json());
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let data =[
+  {
+    "Id" : 1,
+    "name" : "sanjay",
+    "email" : "sanjay@gmail.com",
+    "passward" : "1234@"
+  },{
+    "Id" : 2,
+    "name" : "akash",
+    "email" : "aksah@gmail.com",
+    "passward" : "987@"
+  }
+]
+
+// register
+router.post('/register', function(req, res, next) {
+  let user = {
+    Id : data.length +1,
+    name : req.body.name,
+    email : req.body.email,
+    passward : req.body.passward
+  }
+
+  if(!email || !passward){
+    res.send("invalid details")
+  }else{
+    data.filter(function(detail){
+      if(detail.email === email){
+        res.send("email already exits");
+      }
+    })
+  }
+  data.push(user);
+  res.json(data);
 });
+
+
+
+
+
+
 
 module.exports = router;
