14ff9d885609a2a0f068ee791e40d4687479cf0a
sanjay@crownstack <sanjay.barman@crownstack.com>
2021-05-07 11:33:40 +0530

    crete project and add express generator

---
+++ app.js
@@ -0,0 +1,16 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+app.use('/users', usersRouter);
+
+
+module.exports = app;
