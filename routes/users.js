ee40c19d154e874c111bba0005cd6bf5db56d94d
sanjay@crownstack <sanjay.barman@crownstack.com>
2021-05-10 16:53:22 +0530

    modify all(register,login,delete,read)

--- routes/users.js
+++ routes/users.js
@@ -3,117 +3,100 @@ var router = express.Router();
 
 router.use(express.json());
 /* GET users listing. */
-let data =[
-  {
-    "Id" : 1,
-    "name" : "sanjay",
-    "email" : "sanjay@gmail.com",
-    "passward" : "1234@"
-  },{
-    "Id" : 2,
-    "name" : "akash",
-    "email" : "aksah@gmail.com",
-    "passward" : "987@"
-  }
-]
 
 // register/ signup
-router.post('/register', function(req, res) {
-  let user = {
-    Id : data.length +1,
-    name : req.body.name,
-    email : req.body.email,
-    passward : req.body.passward
-  }
 
-  if(!user.email || !user.passward){
-    res.json("invalid details")
-  }else{
-    data.filter(function(detail){
-      if(detail.email === user.email){
-        res.json("email already exits");
-      }
-    })
-  }
-  data.push(user);
-  res.json(data);
+let usersList=[];
+router.post('/register', function(req, res) {
+  console.log("Users Register Hit!");  
+  /*
+  let userRegisterDetails={
+    name : req.body.name,
+    password : req.body.password,
+    email : req.body.email,
+    mobileNumber:req.body.mobileNumber,
+    dob:req.body.dob
+   }
+  */
+
+  usersList.filter(function(detail){
+    if(detail.email === email){
+      res.json("email already exits try another email");
+    }
+  })
+  usersList.push(req.body);
+  res.json(usersList);
 });
 
 // login 
 router.post('/login', function(req, res){
 
-    email = req.body.email,
-    passward = req. body.passward
-
+  //console.log("userLogin Hit!");
+  let {email,password} = req.body;
 
   if(!email || !passward){
-    res.send("please enter both");
+    res.send("please enter email and passward");
+  }
+  //filter
+  usersList.filter( details => {
+  if(details.email === email && details.password === password){
+    res.json("Login Success");
   }
   else{
-    data.filter(function(detail){detail
-      if(detail.email === email && detail.passward === passward){
-        res.send("logged in succesfully");
-      }
-      else{
-        res.send("wrong id or passward");
-      }
-    })
+    res.json("User not defined")
   }
+  });
 })
 
 //crate notes
-let userNotesList=[
-  { "id":"",
-   "userNotes":""
-  }
-];
+
+let userNotesList=[];
+
 router.post('/createnotes',(req,res)=>{
-console.log("createNotes List");  
-let userNotesObject={
-  id : req.body.id,
-  userNotes : req.body.userNotes
-};
-userNotesList.push(userNotesObject);
-res.json(userNotesList);
+  console.log("createNotes List");  
+  userNotesList.push(req.body);
+  res.json(userNotesList);
 });
 
 
 // read notes
-router.get('/notes/:id', function(req,res,next){
-  let Id = req.params.Id;
-  let index = userNotesList.findIndex((detail)=>{
-    return(detail.Id ==  Number.parseInt(Id));
-  })
-  if(index >= 0){
-    let getValue = data[index]
-    s.note = note;
-    res.send(s.note);
+
+router.get('/readnotes', function(req,res,next){
+
+  if(!email || !passward){
+    res.send("please email and passward");
   }
-  next();
+  res.json(userNotesList);
 })
 
 
 // delete his own note
-router.delete('/deletenotes/:id',(req,res)=>{
+router.delete('/deletenotes/:id',async(req,res)=>{
   console.log("delete User Hit!");
-
   console.log("Before Deletion");
   console.log(userNotesList);
+  let {email}=req.body;
 
-  let getId = req.params.id;
-  let index = userNotesList.findIndex((Notes)=>{
-    return (Notes.id == Number.parseInt(getId))
+  usersList.filter(function(detail){
+    if(detail.email != email){
+      res.json("email not found");
+    }
   });
-  if(index >0){
-    let std = userNotesList[index];
-    userNotesList.splice(index, 1);
-    res.send("deleted");
-  }else{
-    res.send("ID NOT found");
+
+  let filterList;
+  filterList = await userNotesList.filter((notedetails) => {
+  if(notedetails.email != email){
+    console.log(email);
+    return notedetails;
   }
+  // data exchange ->json
+  // browser -> send
   });
-  console.log("after deletion");
-  console.log(data);
+  res.json(filterList);
+  console.log("After deletion");
+  console.log(filterList);
+});
+
 
 
 module.exports = router;
